dd80a5f71e7458181ef57e510e88346e041b8d7f
HassanBahati <mukisabahati@gmail.com>
2020-09-07 13:13:21 +0300

    App structure

--- src/server/controllers/questionsController.js
+++
@@ -1,5 +0,0 @@
-const Questions = require("../src/server/models/schemas/questionsModel");
-
-exports.test = (req  , res)=>{
-    res.send("Greetings from porsche , this is his first perfectly working route and database in mongoose")
-};
